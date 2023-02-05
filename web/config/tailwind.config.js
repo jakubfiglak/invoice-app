f015fd9f07e53f638376e4dda50d056b87f884e1
jakubfiglak <jakub.figlak@netguru.pl>
2023-02-05 21:11:38 +0100

    feat: theme setup

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,6 +2,57 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'lavender-purple': '#7C5DFA',
+      periwinkle: '#9277FF',
+      'night-blue': '#1E2139',
+      'blue-sapphire': '#252945',
+      'light-steel-blue': '#DFE3FA',
+      'light-slate-gray': '#888EB0',
+      'steel-blue': '#7E88C3',
+      'black-russian': '#0C0E16',
+      'coral-red': '#EC5757',
+      'pastel-red': '#FF9797',
+      'white-smoke': '#F8F8FB',
+      eclipse: '#141625',
+    },
+    fontSize: {
+      xs: [
+        '0.6875rem',
+        {
+          lineHeight: '1.125rem',
+          letterSpacing: '-0.02em',
+        },
+      ],
+      sm: [
+        '0.75rem',
+        {
+          lineHeight: '0.9375rem',
+          letterSpacing: '-0.02em',
+        },
+      ],
+      md: [
+        '1rem',
+        {
+          lineHeight: '1.5rem',
+          letterSpacing: '-0.05em',
+        },
+      ],
+      lg: [
+        '1.25rem',
+        {
+          lineHeight: '1.375rem',
+          letterSpacing: '-0.03em',
+        },
+      ],
+      xl: [
+        '2rem',
+        {
+          lineHeight: '2.25rem',
+          letterSpacing: '-0.03em',
+        },
+      ],
+    },
     extend: {
       fontFamily: {
         spartan: ['"League Spartan"', 'sans-serif'],
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,29 +1,46 @@
 import { useAuth } from '@redwoodjs/auth'
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   const { currentUser, logOut } = useAuth()
-  console.log(currentUser)
 
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <div className="mx-auto my-12 max-w-2xl space-y-6">
+        <h1 className="text-xl font-bold">Lorem ipsum dolor sit amet.</h1>
 
-      <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+        <h2 className="text-lg font-bold">Lorem ipsum dolor sit amet.</h2>
 
-      <button className="rw-button rw-button-blue" onClick={logOut}>
-        Logout
-      </button>
+        <h3 className="text-md font-bold">Lorem ipsum dolor sit amet.</h3>
+
+        <h4 className="text-sm font-bold">Lorem ipsum dolor sit amet.</h4>
+
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum odio
+          minima, corrupti accusantium quidem ratione suscipit quod tempore!
+          Similique, voluptatum. Aperiam, porro! Aliquid placeat nesciunt cum a
+          unde, quia quo facilis animi odio, recusandae debitis nam
+          reprehenderit eligendi eius nisi? Odio, accusamus! Esse aut, et eos
+          architecto laboriosam officiis dolores!
+        </p>
+
+        <p className="text-xs">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          illum, autem, molestiae impedit perspiciatis maxime in ab placeat at
+          quas voluptate unde consequatur cum, itaque suscipit assumenda omnis
+          odit obcaecati explicabo aut adipisci alias debitis. Nemo assumenda ex
+          possimus architecto? Adipisci omnis officia sit quisquam iste
+          exercitationem harum et recusandae?
+        </p>
+
+        <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+
+        <button className="rw-button rw-button-blue" onClick={logOut}>
+          Logout
+        </button>
+      </div>
     </>
   )
 }

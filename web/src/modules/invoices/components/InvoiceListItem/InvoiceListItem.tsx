e58a1658a69aa1d878713f0d08f0bb26ae8dcab7
jakubfiglak <jakub.figlak@netguru.pl>
2023-02-18 17:27:49 +0100

    feat: finish styling

--- web/src/modules/invoices/components/InvoiceListItem/InvoiceListItem.tsx
+++ web/src/modules/invoices/components/InvoiceListItem/InvoiceListItem.tsx
@@ -1,10 +1,14 @@
 import { twMerge } from 'tailwind-merge'
 import { InvoiceStatus } from 'types/graphql'
 
+import { Link } from '@redwoodjs/router'
+
 import { getInvoiceNumberFromId } from 'src/modules/invoices/utils/get-invoice-number-from-id'
 import { formatDate, formatMoney } from 'src/utils'
 
 import InvoiceStatusBadge from '../InvoiceStatusBadge'
+
+import Chevron from './chevron.svg'
 
 interface IInvoiceListItemProps {
   id: string
@@ -24,33 +28,40 @@ const InvoiceListItem = ({
   className,
 }: IInvoiceListItemProps) => {
   return (
-    <div
+    <Link
+      // TODO: change this to routes.invoices({ id })
+      to="/"
       className={twMerge(
-        'rounded-lg bg-white p-6 text-sm shadow-md dark:bg-night-blue',
+        'block rounded-lg border-lavender-purple bg-white p-6 text-sm shadow-md @container hover:border focus:border focus:outline-0 dark:bg-night-blue',
         className
       )}
     >
-      <div className="mb-6 flex items-center justify-between">
-        <h4 className="font-bold">
-          <span className="text-steel-blue">#</span>
-          {getInvoiceNumberFromId(id)}
-        </h4>
-        <span className="text-light-slate-gray dark:text-white">
-          {customerName}
-        </span>
-      </div>
-      <div className="flex items-center justify-between">
-        <div>
-          <time className="mb-2 block text-light-slate-gray dark:text-light-steel-blue">
-            Due {formatDate(dueDate)}
-          </time>
-          <span className="block text-md font-bold">
-            {formatMoney(totalAmount)}
+      <div className="justify-between @xl:flex">
+        <div className="mb-6 flex items-center justify-between @xl:mb-0 @xl:gap-8">
+          <h4 className="font-bold">
+            <span className="text-steel-blue">#</span>
+            {getInvoiceNumberFromId(id)}
+          </h4>
+          <span className="text-light-slate-gray dark:text-white">
+            {customerName}
           </span>
         </div>
-        <InvoiceStatusBadge status={status} className="w-28" />
+        <div className="flex items-center justify-between @xl:gap-10">
+          <div className="items-center @xl:flex @xl:gap-10">
+            <time className="mb-2 block text-light-slate-gray @xl:mb-0 dark:text-light-steel-blue">
+              Due {formatDate(dueDate)}
+            </time>
+            <span className="block text-md font-bold">
+              {formatMoney(totalAmount)}
+            </span>
+          </div>
+          <div className="items-center gap-4 @xl:flex">
+            <InvoiceStatusBadge status={status} className="w-28" />
+            <Chevron className="hidden @xl:block" />
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 

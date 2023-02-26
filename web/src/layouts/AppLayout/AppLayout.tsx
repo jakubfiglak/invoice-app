2ebcb304981876fb4fa3d4c0b51cce6803b3c853
jakubfiglak <jakub.figlak@netguru.pl>
2023-02-26 15:41:21 +0100

    feat: style adjustments for AppLayout

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -10,7 +10,7 @@ const AppLayout = ({ children }: IAppLayoutProps) => {
   return (
     <>
       <Header className="fixed top-0 left-0 w-full" />
-      <main className="mt-[72px] px-6 py-8 sm:mt-20 sm:px-12 sm:py-14 lg:mx-auto lg:max-w-[730px] lg:px-0">
+      <main className="mt-[72px] px-6 pb-8 sm:mt-20 sm:px-12 sm:pb-14 lg:mx-auto lg:max-w-[730px] lg:px-0">
         {children}
       </main>
     </>
